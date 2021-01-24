8146b5b9831619d4e4d65133dc2e2172d015dfa3
akouddous <akouddous.git@gmail.com>
2021-01-24 12:03:38 +0000

    Sections 1 to 40

---
+++ src/Components/App/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [{name: 'name1', artist: 'artist1', album: 'album1', id: 1 }, {name: 'name2', artist: 'artist2', album: 'album2', id: 2 } ],
+      playlistName: 'playlistName1',
+      playlistTracks: [{name: 'playlistName1', artist: 'playlistArtist1', album: 'playlistAlbum1', id: 3}]}
+  }
+
+  addTrack(track) {
+    let tracks 
+
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults searchResults={this.state.searchResults} />
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}/>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
