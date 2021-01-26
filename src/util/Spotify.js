b5121ca8d4a164e86d76a28535a6325fc8bba3d0
akouddous <akouddous.git@gmail.com>
2021-01-26 14:45:52 +0000

    Implemented Spotify Seach Request

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -4,10 +4,39 @@ let accessToken;
 
 const Spotify = {
 
+    search(term) {
+        const accessToken = Spotify.getAccessToken();
+        return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        }).then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error('Response failed!');
+        }, networkError => console.log(networkError.message)
+        ).then(jsonResponse => {
+            if (!jsonResponse.tracks) {
+                return []
+            }
+            return jsonResponse.tracks.items.map(track => ({
+                id: track.id,
+                name: track.name,
+                artist: track.artist[0].name,
+                album: track.album.name,
+                uri: track.uri,
+            }))
+
+        })
+    },
+
+
     getAccessToken() {
         if (accessToken) {
-            return accessToken;
+            return accessToken
         }
+    
 
         //check for access token match
         const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
@@ -24,10 +53,8 @@ const Spotify = {
             const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
             window.location = accessUrl;
 
-        }
-    }
-
-
+        }}
+    
 }
 
 export default Spotify
